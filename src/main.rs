5ab78a21213a9a529a09d551f20ae86e72a62859
HighOnPlutonium <74098648+HighOnPlutonium@users.noreply.github.com>
2025-02-28 14:47:44 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::marker::PhantomData;
+use std::ptr;
+use ash::Entry;
+use ash::vk;
+
 fn main() {
-    println!("Hello, world!");
+    let entry = unsafe { Entry::load().unwrap() };
+    let app_info = vk::ApplicationInfo {
+        p_application_name: "placeholder name".as_ptr().cast(),
+        //application_version: 0,
+        //p_engine_name: (),
+        //engine_version: 0,
+        api_version: vk::make_api_version(0,1,0,0),
+        ..Default::default()
+    };
+    
+    let create_info = vk::InstanceCreateInfo {
+
+        p_application_info: &app_info,
+        //enabled_layer_count: 0,
+        //pp_enabled_layer_names: (),
+        //enabled_extension_count: 0,
+        //pp_enabled_extension_names: (),
+        ..Default::default()
+    };
+    
+    let instance = unsafe { entry.create_instance(&create_info, None).unwrap() };
 }
